884d5c89e85c151249ded9bcf67792501178d600
world <world@Veroooooooo>
2024-11-07 18:24:16 -0600

    se genero repositorio de empresa

--- src/main/java/com/abarrotes/service/EmpresaService.java
+++ src/main/java/com/abarrotes/service/EmpresaService.java
@@ -8,70 +8,131 @@ import com.abarrotes.dto.EmpresaDto;
 import com.abarrotes.entidad.Empresa;
 import com.abarrotes.repository.EmpresaRepository;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
  *
  * @author world
  */
-
 @Service
 
-public class EmpresaService {
+public class EmpresaService implements UserDetailsService {
+    
     @Autowired
     EmpresaRepository empresaRepository;
-    public List<Empresa> findAll(){
-       List<Empresa> lstEmpresaDto = empresaRepository.findAll();
-       return lstEmpresaDto;
-    }
-    private EmpresaDto converterEntidadDto (EmpresaDto e){
-    EmpresaDto empresaDto = new EmpresaDto();
-    empresaDto.setCorreo(e.getCorreo());
-    empresaDto.setDireccion(e.getDireccion());
-    empresaDto.setEnvioDeMensajes(e.getEnvioDeMensajes());
-    empresaDto.setEstatus(e.getEstatus());
-    empresaDto.setHost(e.getHost());
-    empresaDto.setIdEmpresaPk(e.getIdEmpresaPk());
-    empresaDto.setInstancia(e.getInstancia());
-    empresaDto.setLogo(e.getLogo());
-    empresaDto.setNombre(e.getNombre());
-    empresaDto.setNombreComercial(e.getNombreComercial());
-    empresaDto.setNumeroDeUsuarios(e.getNumeroDeUsuarios());
-    empresaDto.setPasswordCorreo(e.getPasswordCorreo());
-    empresaDto.setPuerto(e.getPuerto());
-    empresaDto.setRepresentanteLegal(e.getRepresentanteLegal());
-    empresaDto.setSucursalList(e.getSucursalList());
-    empresaDto.setTelefono(e.getTelefono());
-    empresaDto.setToken(e.getToken());
-    empresaDto.setUsuarioCorreo(e.getUsuarioCorreo());
     
-    return empresaDto;
+    public List<EmpresaDto> findAllNativo() {
+        List<EmpresaDto> lstEmpresaDto = new ArrayList<>();
+        List<Empresa> lstEmpresa = empresaRepository.findAllNativo();
+        for (Empresa e : lstEmpresa) {
+            EmpresaDto ed = converterEntidadDto(e);
+            lstEmpresaDto.add(ed);
+        }
+        return lstEmpresaDto;
     }
     
-    private Empresa converterEntidadDto (Empresa e){
-    Empresa empresa = new Empresa ();
-    empresa.setCorreo(e.getCorreo());
-    empresa.setDireccion(e.getDireccion());
-    empresa.setEnvioDeMensajes(e.getEnvioDeMensajes());
-    empresa.setEstatus(e.getEstatus());
-    empresa.setHost(e.getHost());
-    empresa.setIdEmpresaPk(e.getIdEmpresaPk());
-    empresa.setInstancia(e.getInstancia());
-    empresa.setLogo(e.getLogo());
-    empresa.setNombre(e.getNombre());
-    empresa.setNombreComercial(e.getNombreComercial());
-    empresa.setNumeroDeUsuarios(e.getNumeroDeUsuarios());
-    empresa.setPasswordCorreo(e.getPasswordCorreo());
-    empresa.setPuerto(e.getPuerto());
-    empresa.setRepresentanteLegal(e.getRepresentanteLegal());
-    empresa.setSucursalList(e.getSucursalList());
-    empresa.setTelefono(e.getTelefono());
-    empresa.setToken(e.getToken());
-    empresa.setUsuarioCorreo(e.getUsuarioCorreo());
+    public EmpresaDto insert(EmpresaDto e) {
+        return converterEntidadDto(empresaRepository.save(converterDtoEntidad(e)));
+        
+    }
     
-    return empresa;
+    public EmpresaDto update(EmpresaDto e) {
+        return converterEntidadDto(empresaRepository.save(converterDtoEntidad(e)));
+        
+    }
+    
+    public EmpresaDto findByIdUsuario(Integer idUsuario) {
+        EmpresaDto e = new EmpresaDto();
+//        Empresa emp = empresaRepository.findByIdUsuario(idUsuario);
+//        e = converterEntidadDto(emp);
+//
+//        return e;
+        List<Object[]> lstO = empresaRepository.findByIdUsuario(idUsuario);
+        
+        for (Object[] o : lstO) {
+            e.setIdEmpresaPk(Integer.parseInt(o[0].toString()));
+            e.setNombre(o[1].toString());
+            e.setCorreo(o[2].toString());
+            e.setDireccion(o[2] == null ? null:o[2].toString());
+            
+        }
+        
+        return e;
+    }
+    
+    public int delete(EmpresaDto e) {
+//        return empresaRepository.delete(e.getIdEmpresaPk(), '0');
+        return 0;
+    }
+    
+    private EmpresaDto converterEntidadDto(Empresa e) {
+        EmpresaDto empresaDto = new EmpresaDto();
+        empresaDto.setCorreo(e.getCorreo());
+        empresaDto.setDireccion(e.getDireccion());
+        empresaDto.setEnvioDeMensajes(e.getEnvioDeMensajes());
+        empresaDto.setEstatus(e.getEstatus());
+        empresaDto.setHost(e.getHost());
+        empresaDto.setIdEmpresaPk(e.getIdEmpresaPk());
+        empresaDto.setInstancia(e.getInstancia());
+        empresaDto.setLogo(e.getLogo());
+        empresaDto.setNombre(e.getNombre());
+        empresaDto.setNombreComercial(e.getNombreComercial());
+        empresaDto.setNumeroDeUsuarios(e.getNumeroDeUsuarios());
+        empresaDto.setPasswordCorreo(e.getPasswordCorreo());
+        empresaDto.setPuerto(e.getPuerto());
+        empresaDto.setRepresentanteLegal(e.getRepresentanteLegal());
+        empresaDto.setSucursalList(e.getSucursalList());
+        empresaDto.setTelefono(e.getTelefono());
+        empresaDto.setToken(e.getToken());
+        empresaDto.setUsuarioCorreo(e.getUsuarioCorreo());
+        
+        return empresaDto;
+    }
+    
+    private Empresa converterDtoEntidad(EmpresaDto e) {
+        Empresa empresa = new Empresa();
+        empresa.setCorreo(e.getCorreo());
+        empresa.setDireccion(e.getDireccion());
+        empresa.setEnvioDeMensajes(e.getEnvioDeMensajes());
+        empresa.setEstatus(e.getEstatus());
+        empresa.setHost(e.getHost());
+        empresa.setIdEmpresaPk(e.getIdEmpresaPk());
+        empresa.setInstancia(e.getInstancia());
+        empresa.setLogo(e.getLogo());
+        empresa.setNombre(e.getNombre());
+        empresa.setNombreComercial(e.getNombreComercial());
+        empresa.setNumeroDeUsuarios(e.getNumeroDeUsuarios());
+        empresa.setPasswordCorreo(e.getPasswordCorreo());
+        empresa.setPuerto(e.getPuerto());
+        empresa.setRepresentanteLegal(e.getRepresentanteLegal());
+        empresa.setSucursalList(e.getSucursalList());
+        empresa.setTelefono(e.getTelefono());
+        empresa.setToken(e.getToken());
+        empresa.setUsuarioCorreo(e.getUsuarioCorreo());
+        
+        return empresa;
+    }
+
+//    public void insert(EmpresaDto empresa) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    public void update(EmpresaDto empresa) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    public void delete(EmpresaDto empresaDelete) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
